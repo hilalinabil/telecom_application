e34d42530d141612cf467dfc6fd2679a08476157
nabil <nabil>
2026-06-16 15:45:06 +0100

    Intial commit setup springboot project

---
+++ src/main/java/com/example/telecom_network/TelecomNetworkApplication.java
@@ -0,0 +1,13 @@
+package com.example.telecom_network;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelecomNetworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelecomNetworkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/telecom_network/TelecomNetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.telecom_network;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelecomNetworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
